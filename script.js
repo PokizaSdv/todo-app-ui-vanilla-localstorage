e5ff10d01ba5631d986931143edba874796261b3
PokizaSdv <pokizasdv@gmail.com>
2023-12-08 02:55:56 -0500

    Create Storage Class and it's methods for managing todos

---
+++ script.js
@@ -0,0 +1,35 @@
+class Storage {
+    get() {
+        const todos = localStorage.getItem("todos");
+        if (!todos) return [];
+        return JSON.parse(todos);
+    }
+
+    setTodos(updatedTodos) {
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+
+    add(todo) {
+        const existingTodos = this.get();
+        existingTodos.push(todo);
+        this.setTodos(existingTodos);
+    }
+
+    delete(id) {
+        const existingTodos = this.get();
+        const keptTodos = existingTodos.filter((todo) => todo.id !== id);
+        this.setTodos(keptTodos);
+    }
+
+    changeStatus(id, status) {
+        const existingTodos = this.get();
+        for (const todo of existingTodos) {
+            if (todo.id === id) {
+                todo.status = status;
+            }
+        }
+        this.setTodos(existingTodos);
+    }
+}
+
+const storage = new Storage();
